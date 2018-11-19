4af597270294ba6b18eaeef69eed026b0d13b4de
Ed Huamantla <edgar@huamantla.biz>
2018-11-18 21:07:27 -0500

    printing what is returned by dictionary.items

--- scratchBook/dictionaries.py
+++ scratchBook/dictionaries.py
@@ -13,7 +13,20 @@ dictionary = {'Andrew Chalkley': ['jQuery Basics', 'Node.js Basics'],
 # for teacher in enumerate(dictionary):
 #     print(teacher)
 
+# for teacher in dictionary.items():
+#     print(teacher, 'is of ', type(teacher), '1st loop') # returns a tuple
+#     for course in teacher:
+#         print(course, 'is of', type(course), ' 2nd loop') # goes through the tuple index 0,...
+
+
+# most_booked = ""
+# counter = 0
+#
+# for teacher in dictionary.items():
+#     if len(teacher[1]) >= counter:
+#         most_booked = teacher[0]
+
+list_teacher_course = []
+
 for teacher in dictionary.items():
-    print(teacher, 'is of ', type(teacher), '1st loop') # returns a tuple
-    for course in teacher:
-        print(course, 'is of', type(course), ' 2nd loop') # goes through the tuple index 0,...
+    list_teacher_course.append([teacher[0], len(teacher[1])])
