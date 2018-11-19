c6ac61fd6215e649592108efccc6082b9f26e40d
Ed Huamantla <edgar@huamantla.biz>
2018-11-18 20:40:53 -0500

    added print statements of dictionary methods

--- scratchBook/dictionaries.py
+++ scratchBook/dictionaries.py
@@ -1,6 +1,17 @@
 dictionary = {'Andrew Chalkley': ['jQuery Basics', 'Node.js Basics'],
      'Kenneth Love': ['Python Basics', 'Python Collections']
     }
+# # print(len(dictionary))
+#
+#
+# for teacher in dictionary.keys():
+#     print(teacher)
+#
+# for teacher in dictionary.values():
+#     print(teacher)
+#
+# for teacher in enumerate(dictionary):
+#     print(teacher)
 
 for teacher in dictionary.items():
     print(teacher, 'is of ', type(teacher), '1st loop') # returns a tuple
