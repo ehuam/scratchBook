32ac5387613187b58b7da021ac80b5eb0a629024
edgar@huamantla <edgar@huamantla.biz>
2018-11-26 16:21:12 -0500

    treehouse slices with colon

---
+++ scratchBook/treehouseSlices.py
@@ -0,0 +1,8 @@
+favorite_things = ['raindrops on roses',
+                   'whiskers on kittens',
+                   'bright copper kettles',
+                   'warm woolen mittens',
+                   'bright paper packages tied up with string',
+                   'cream colored ponies',
+                   'crisp apple strudels',
+                  ]
