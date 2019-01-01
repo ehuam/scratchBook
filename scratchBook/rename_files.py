2f89a3105de1e5d384b4a32e9e477777108f11ea
Ed Huamantla <edgar@huamantla.biz>
2019-01-01 16:33:48 -0500

    task: added an index to be appended to file names.

--- scratchBook/rename_files.py
+++ scratchBook/rename_files.py
@@ -1,6 +1,10 @@
 #! /usr/bin/python3
 
 import os
+index = 1
 for filename in os.listdir("."):
-    if filename.startswith("cheese_"):
-        os.rename(filename, filename[7:])
+    if filename.startswith("."): # so that private files aren't renamed.
+        pass
+    else:
+        os.replace(filename, (index + '_' + filename)) 
+    index += 1
