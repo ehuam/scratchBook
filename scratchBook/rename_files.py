b87c9e3b04bcabbe85da4b8a42161a37a6e48f28
Ed Huamantla <edgar@huamantla.biz>
2019-01-01 16:52:18 -0500

    chore: added to string documentation

--- scratchBook/rename_files.py
+++ scratchBook/rename_files.py
@@ -3,8 +3,13 @@
 """"
 code snippet to rewrite files in a directory.
 
-current bug: if there are hidden files, the index keeps counting. 
-1/1/19: fixing the bug by only incrementing the index if it's not a hidden file (a file starting with a period)
+bug: if there are hidden files, the index keeps counting. Fixed
+        1/1/19: fixing the bug by only incrementing the index if it's not a hidden file (a file starting with a period)
+
+issue: renaming files keeps prefixing, should be someway to only run once, and somehow also be able store what it was before.
+        potential solution: creating a text file that has the original names.
+
+
 """
 
 import os
