b45287519c10dfe6695e64012df4181b0882704f
Ed Huamantla <edgar@huamantla.biz>
2019-01-01 16:41:56 -0500

    feature: changed to only increment the index if filename doesn't start with a period.

--- scratchBook/rename_files.py
+++ scratchBook/rename_files.py
@@ -1,5 +1,12 @@
 #! /usr/bin/python3
 
+""""
+code snippet to rewrite files in a directory.
+
+current bug: if there are hidden files, the index keeps counting. 
+1/1/19: fixing the bug by only incrementing the index if it's not a hidden file (a file starting with a period)
+"""
+
 import os
 index = 1
 for filename in os.listdir("."):
@@ -7,4 +14,5 @@ for filename in os.listdir("."):
         pass
     else:
         os.replace(filename, (index + '_' + filename)) 
-    index += 1
+        index += 1
+    
