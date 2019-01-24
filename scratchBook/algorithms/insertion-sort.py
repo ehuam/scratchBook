5085764cc3e6b29f52b94fde30fccb807178186e
edgar@huamantla <edgar@huamantla.biz>
2019-01-23 20:35:01 -0500

    added: added print line statements that display values to screen as the program goes through its loops

--- scratchBook/algorithms/insertion-sort.py
+++ scratchBook/algorithms/insertion-sort.py
@@ -5,12 +5,17 @@
 array = [2, 4, 6, 5, 1, 3]
 print(array)
 for j in range(1, len(array)):
+    print('I am in loop {}'.format(j))
     key = array[j]
+    print('the value of the key is {}'.format(key))
     # insert A[j] into the sorted sequence A[1...j-1]
     i = j - 1
-    # print('the value of the key is {}'.format(key))
+    print('the value of i is {} and j is {}'.format(i,j))
     while i >= 0 and array[i] > key:
         array[i+1] = array[i] 
+        print('the value at position {} is being set to {}'.format(i, array[i+1]))
         i = i - 1
+        print('the value of i is being set to {}'.format(i))
     array[i+1] = key
+    print('the value at position {index} is being set to the key value of {keyValue}'.format(index=i+1, keyValue =key))
 print(array)
