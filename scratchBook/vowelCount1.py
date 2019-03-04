d286a60702e8726f82ad6b4fea255f054c78e3d7
Ed Huamantla <edgar@huamantla.biz>
2019-03-03 20:55:16 -0500

    small fix. made variable word that takes user input from the command line to be stored as a lower case.

--- scratchBook/vowelCount1.py
+++ scratchBook/vowelCount1.py
@@ -3,9 +3,9 @@ vowels = ['a','e','i','o','u',]
 print("Vowel search: Enter 'stop' to break out of the loop")
 
 while True:
-        word = input("Provide your word to search for a vowel\n")
+        word = input("Provide your word to search for a vowel\n").lower()
         
-        if word.lower() == 'stop':
+        if word == 'stop':
                 break
         
         found = {vowel: word.count(vowel) for vowel in word if vowel in vowels} 
