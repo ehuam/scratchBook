cea65a512999aad4f4503ec9699226ab73753c54
Ed Huamantla <edgar@huamantla.biz>
2019-03-24 20:17:32 -0400

    updating vowelCount2.py to use a set and a set comprehension to count vowels

--- scratchBook/vowelCount2.py
+++ scratchBook/vowelCount2.py
@@ -1,4 +1,4 @@
-vowels = set('a','e','i','o','u')
+vowels = {'a','e','i','o','u'}
 
 print("Vowel search: Enter 'stop' to break out of the loop")
 
@@ -7,5 +7,11 @@ while True:
         
         if word == 'stop':
                 break
-        
-        found = {vowel: word.count(vowel) for vowel in word if vowel in vowels} 
+
+        # using a set and intersection to find commom letters between the sets
+        found = vowels.intersection(set(word))
+
+        # using a set comprehension 
+        #  val for val in set if vowel in set of vowels
+        # found = {vowel: word.count(vowel) for vowel in word if vowel in vowels} 
+        print(found)
