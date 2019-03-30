a01285707b7c3e36f395dcaa2e660873ffc23883
Ed Huamantla <edgar@huamantla.biz>
2019-03-30 18:50:41 -0400

    feature: added search4letters function

--- scratchBook/headFirst/vsearch.py
+++ scratchBook/headFirst/vsearch.py
@@ -4,3 +4,7 @@ def search4vowels(phrase: str) -> set():
         """
         vowels = set ('aeiou')
         return vowels.intersection(set(phrase))
+
+def search4letters(phrase:str, letters: str) -> set:
+        # convert the letters input to a set to do set operations
+        return set(letters).intersection(set(phrase))
