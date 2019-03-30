324fbcc710f81a5fe786415918eb8977bb5318f2
edgar@huamantla <edgar@huamantla.biz>
2019-03-30 07:56:00 -0400

    feature to vsearch.py: added function annotations. teh syntax is almost like setting up a dictionary in the argument of the function. in this examle its 'word:str', variable:type followed by a dash and less than ( - > ) so that it looks like an arrow followed by what the function returns. in this function it returns an empty set so the annotation is like so: def search4vowels(word:str) -> set():

--- scratchBook/headFirst/vsearch.py
+++ scratchBook/headFirst/vsearch.py
@@ -1,4 +1,4 @@
-def search4vowels(string):
+def search4vowels(word: str) -> set():
         """ Display any vowels found in an asked-for word. 
             Returns an empy set if not found. Set()        
         """
