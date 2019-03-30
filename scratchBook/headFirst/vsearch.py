6f6cec26e99b774819a8a31ec28802e36c6aec7b
Ed Huamantla <edgar@huamantla.biz>
2019-03-30 18:58:53 -0400

    updated docstring comments to search4letters

--- scratchBook/headFirst/vsearch.py
+++ scratchBook/headFirst/vsearch.py
@@ -7,9 +7,11 @@ def search4vowels(phrase: str) -> set():
 
 def search4letters(phrase:str, letters: str) -> set:
         """
-        convert the letters input to a set to do set operations
-        we don't need to assign the set object to a variable becase
-        we are more interested in using the set of letters right
-        away than in storing the set in a variable for later use
+        Return a set of the 'letters' found in 'phrase
         """
+        # convert the letters input to a set to do set operations
+        # we don't need to assign the set object to a variable becase
+        # we are more interested in using the set of letters right
+        # away than in storing the set in a variable for later use
+        
         return set(letters).intersection(set(phrase))
