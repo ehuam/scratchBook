d69de9875668cfbb0063e4ba9ee9747321d9d0ce
Edgar Huamantla <Edgar@maclaptop>
2019-04-04 21:08:30 -0400

    fix: changed @app.route decorater for url /search4 to support the post method

--- scratchBook/headFirst/webapp/hello_flask.py
+++ scratchBook/headFirst/webapp/hello_flask.py
@@ -10,14 +10,14 @@ app = Flask(__name__)
 def hello() -> str:
     return 'Hello world from Flask!'
 
-@app.route('/search4')
+@app.route('/search4_ed')
 def do_search() -> str:
     phrase = 'life, the universe, and everything'
     letters = 'eiru,!'
     results = ''
     return results.join(search4letters(phrase=phrase, letters=letters))
 
-@app.route('/search4_hf')
+@app.route('/search4', methods=['POST'])
 def do_search_hf() -> str:
     # returns a string representation of a set
     return str(search4letters('life, the universe, and everything', 'eiru,!'))
