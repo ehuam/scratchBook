8aa229c4de86ccbe91751bc82f2692e834bfed41
Edgar Huamantla <Edgar@maclaptop>
2019-04-07 14:14:09 -0400

    Updated log_request to print(str(dir(req))). Caling dir on req produces a list then we translate it into a string and saved in the log file.

--- scratchBook/headFirst/webapp/vsearch4web.py
+++ scratchBook/headFirst/webapp/vsearch4web.py
@@ -1,9 +1,14 @@
 
 
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request, escape 
 from vsearch import search4letters
 
 app = Flask(__name__)
+
+
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log','a') as log:
+        print(str(dir(req)), res,file=log)
 
 
 @app.route('/search4', methods=['POST'])
@@ -12,19 +17,26 @@ def do_search() -> 'html':
     letters = request.form['letters']
     title = 'Hare are your results'
     results = str(search4letters(phrase, letters))
-
+    log_request(request, results)
+    
     return render_template('results.html', 
                            the_phrase=phrase,
                            the_letters=letters,
                            the_title=title,
                            the_results=results,)
 
-
+    
 @app.route('/')
 @app.route('/entry', methods=['GET'])
 def entry_page() -> 'html':
     return render_template('entry.html',
                             the_title='Welcome to search4letters on the web!')
+
+@app.route('/viewlog')
+def view_the_log() -> str:
+    with open('vsearch.log','r') as log:
+        contents = log.read()
+    return escape(contents)
 
 if __name__ == '__main__':
     app.run(debug=True)
