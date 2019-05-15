43104ed6be48998565c478e8e027e1e04d19f85f
Edgar Huamantla <Edgar@maclaptop>
2019-05-14 22:44:51 -0400

    implemented __repr__ to return attribute value 'val'

--- scratchBook/headFirst/CountFromBy.py
+++ scratchBook/headFirst/CountFromBy.py
@@ -2,12 +2,12 @@ class CountFromBy:
     def __init__(self, v: int=0, i: int=1) -> None:
         self.__val = v
         self.__incr = i
-        
+
     def increase(self) -> None:
         self.__val += self.__incr
 
-    def __str__(self) -> str:
-        return str(self.__val)
+#    def __str__(self) -> str:
+#       return str(self.__val)
 
     @property
     def val(self):
@@ -15,8 +15,11 @@ class CountFromBy:
         return self.__val
 
     @val.setter
-    def val(self, value:int):
+    def val(self, value: int):
         if value < 0:
             raise ValueError("Value must be greater than 0")
         print("Setting Value")
         self.__val = value
+
+    def __repr__(self) -> str:
+        return str(self.__val)
