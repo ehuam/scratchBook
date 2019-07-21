b11da01f704c160362091cea4f62510c0c7fa4a5
Edgar Huamantla <edgar@macmini>
2019-07-21 11:20:17 -0400

    outerInner: to describe how inner functions can be nested inside an outer function and the outer function invoking/calling or returning the function object.

---
+++ scratchBook/outerInner.py
@@ -0,0 +1,23 @@
+""" simple program to highlight nested functions"""
+
+def main():
+    
+def outerInvoke():
+    """Inner function nested inside of Outer.
+       Inner function is fuction is invoked from outer"""
+    def inner():
+        print('This is inner.')
+    
+    print('This is outer, invoking inner.')
+    inner()
+    
+def outerReturn():
+    """ Same as outerInvoke except that inner is returned"""
+    def inner():
+        print('This is inner.')
+    
+    print('This is outer, returning inner.')
+    return inner # without parenthesis the function is returned
+
+if __name__ == "__main__":
+    main()
