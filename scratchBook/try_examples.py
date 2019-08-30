4241ce2339570c5a1d0ddc60b4fcf4f3859de45e
Edgar Huamantla <edgar@huamantla.biz>
2019-08-30 16:46:17 -0400

    try_examples and creating custom exceptions. Using parent "Exceptions" and raise function"

--- scratchBook/try_examples.py
+++ scratchBook/try_examples.py
@@ -14,3 +14,13 @@ except Exception as err:
     print('Some other error occured.', str(err))
     error = sys.exc_info()
     print('using sys module:', error )
+
+class ConnectionError(Exception):
+    pass
+
+# raise ConnectionError('Cannot connect... is it time to panic?')
+
+try:
+    raise ConnectionError('Whoops')
+except ConnectionError as err:
+    print('Got:', str(err))
