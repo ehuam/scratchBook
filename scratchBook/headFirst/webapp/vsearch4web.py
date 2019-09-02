3e967a3cc6a599e8595c90d1177eb3b744be8b0a
Edgar Huamantla <huamantlae@gmail.com>
2019-09-02 11:51:18 -0400

    debug: added try/catch handlers for custom errors ConnectionError and CredentialsError.

--- scratchBook/headFirst/webapp/vsearch4web.py
+++ scratchBook/headFirst/webapp/vsearch4web.py
@@ -97,8 +97,13 @@ def view_the_log() -> 'html':
                                the_title='View Log',
                                the_row_titles=titles,
                                the_data=contents,)
+    except ConnectionError as err:
+        print('Is your database switched on? Error: ', str(err))
+    except CredentialsError as err:
+        print('User-id/Password issues. Error: ', str(err))
     except Exception as err:
         print('Something went wrong:', str(err))
+    return 'Error'
 
 if __name__ == '__main__':
     app.run(debug=True)
