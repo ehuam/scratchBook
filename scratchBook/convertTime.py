771d74de302a821d2bb6b9c4543949d4217a1b9b
Edgar Huamantla <edgar@huamantla.biz>
2019-09-28 23:50:08 -0400

    added dictcomp

--- scratchBook/convertTime.py
+++ scratchBook/convertTime.py
@@ -18,5 +18,9 @@ with open('buzzers.csv') as data:
 
     for time, flight in flights.items():
         flights2[convert2ampm(time)] = flight.title() 
-
+    print('printing out flights2')
     pprint.pprint(flights2)
+
+    more_flights = {convert2ampm(k):v.title() for k,v in flights.items()}
+    print('printing out more_flights')
+    pprint.pprint(more_flights)
