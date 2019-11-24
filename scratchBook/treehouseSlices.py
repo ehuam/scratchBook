42f790e2b6ff7385327db6aac587c11cc23eac43
Edgar Huamantla <edgar@huamantla.biz>
2019-11-24 15:34:14 -0500

    changing directory

--- scratchBook/treehouseSlices.py
+++
@@ -1,8 +0,0 @@
-favorite_things = ['raindrops on roses',
-                   'whiskers on kittens',
-                   'bright copper kettles',
-                   'warm woolen mittens',
-                   'bright paper packages tied up with string',
-                   'cream colored ponies',
-                   'crisp apple strudels',
-                  ]
